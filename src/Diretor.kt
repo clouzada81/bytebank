d6aecbb2779af79287b2ef3c1b0d507f84328279
= <clouzada81@hotmail.com>
2021-04-25 18:59:21 -0300

    Heranca

---
+++ src/Diretor.kt
@@ -0,0 +1,15 @@
+class Diretor(
+    nome: String,
+    cpf: String,
+    salario: Double,
+    var plr: Double
+) : Funcionario(
+    nome = nome,
+    cpf = cpf,
+    salario = salario
+) {
+
+    override fun bonificacao(): Double {
+        return salario * 0.3
+    }
+}
---
+++ src/Gerente.kt
@@ -0,0 +1,14 @@
+class Gerente(
+    nome: String,
+    cpf: String,
+    salario: Double,
+) : Funcionario(
+    nome = nome,
+    cpf = cpf,
+    salario = salario
+) {
+
+    override fun bonificacao(): Double {
+        return salario * 0.2
+    }
+}
